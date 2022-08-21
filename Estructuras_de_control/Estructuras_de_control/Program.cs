a2bc5fabc32fd64b11b180f37431e10d8ce493b1
RyanEsc <110208605+Bybryanx1997@users.noreply.github.com>
2022-08-21 08:56:19 -0600

    Estructura_de_control

---
+++ Estructuras_de_control/Estructuras_de_control/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int entero;
+            string SoN;
+
+            Console.Write("si quieres comvertir a minutos escriba -1-, si quieresconvertir a segundos escriba -2- ");
+            SoN = (Console.ReadLine());
+
+            Console.Write("Escriba la cantidad de horas que desea convertir : \n");
+            entero = int.Parse(Console.ReadLine());
+            if (SoN == "1")
+            {
+                Console.Write("El resultado en minutos es de: " + (entero * 60));
+            }
+            if (SoN == "2")
+            {
+                Console.Write("El resultado en segundos es de: " + (entero * 60 * 60));
+
+            }
+            Console.Read();
+        }
+    }
